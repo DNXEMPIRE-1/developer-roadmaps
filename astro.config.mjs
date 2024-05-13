a1fd7cbb7e2825eda3775a484771fc54a17630fa
“Dennis <dennisnzioki019@gmail.com>
2024-05-13 23:05:08 +0300

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,60 @@
+// https://astro.build/config
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
+import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links';
+import { serializeSitemap, shouldIndexPage } from './sitemap.mjs';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://roadmap.sh/',
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+    },
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          rel: function (element) {
+            const href = element.properties.href;
+            const whiteListedStarts = [
+              '/',
+              '#',
+              'mailto:',
+              'https://github.com/kamranahmedse',
+              'https://thenewstack.io',
+              'https://kamranahmed.info',
+              'https://roadmap.sh',
+            ];
+            if (whiteListedStarts.some((start) => href.startsWith(start))) {
+              return [];
+            }
+            return 'noopener noreferrer nofollow';
+          },
+        },
+      ],
+    ],
+  },
+  output: 'hybrid',
+  adapter: node({
+    mode: 'standalone',
+  }),
+  trailingSlash: 'never',
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    sitemap({
+      filter: shouldIndexPage,
+      serialize: serializeSitemap,
+    }),
+    react(),
+  ],
+});
